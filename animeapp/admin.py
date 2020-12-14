0e0c6a130b4b35d972191497ab51e759c621506e
Miguel Guerrero Luna <miguel.guerreroluna1999@gmail.com>
2020-12-14 10:38:40 +0100

    animetakuv4

--- animeapp/admin.py
+++ animeapp/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
-from .models import anime
-from .models import user
+from .models import anime, user, anime_review
 
+class AnimeAdmin(admin.ModelAdmin):
+    fields = ['name_anime','release_date','genres']
 
+class userInline(admin.ModelAdmin):
+    fieldsets = [
+        (None,  {'fields': ['name_user']}),
+        ('Date information', {'fields': ['email', 'classes': ['collapse']]})
+    ]
+    inline = [userInline]
 # Register your models here.
 
-admin.site.register(anime)
-admin.site.register(user)
+admin.site.register(anime, AnimeAdmin)
+admin.site.register(user,userInline)
--- animeapp/views.py
+++ animeapp/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import get_object_or_404,render
-from django.http import HttpResponse
-from .models import anime
+from django.http import HttpResponseRedirect, Http404
+from .models import anime, user, review
+from django.urls import reverse
 from django.template import loader
+from django.views import generic
 
-def index(request):
+def index(anime,request):
     latest_anime = anime.objects.order_by('-release_date')[:5]
     context={'latest_anime': latest_anime}
-    return render(request, 'index.html')
+    return render(request, 'index.html', context)
 
 def user_registered(request, user_id):
-    info = get_obj_or_404(user, pk=user_id)
-    return render(request, 'detail.html', {'name': name})
+    info = get_object_or_404(user, pk=user_id)
+    return render(request, 'detail.html', {'name': info})
 
 def anime_review(request, review_id):
-    anime_item = get_object_or_404(anime, spk=name_anime)
+    anime_item = get_object_or_404(anime, spk=review_id)
     try:
         selected_review = review.text_post.get(pk=request.POST['review'])
-    except (KeyError, Choice.DoesNotExist):
+    except (KeyError, anime_item.DoesNotExist):
         return render(request, 'animeapp/detail.html', {
             'anime': anime,
             'error_message': "You didn't select a choice.",
@@ -24,4 +26,17 @@ def anime_review(request, review_id):
     else:
         selected_review.votes += 1
         selected_review.save()
-    return HttpResponseRedirect(reverse('animeapp:reviews', args=(review.user.post)))
+    return HttpResponseRedirect(reverse('animeapp:reviews', args=(review_id.user.post)))
+
+class IndexView(generic.ListView):
+    template_name = "animeapp/index.html"
+    context_object_name = 'latest_anime'
+    def get_queryset(self):
+        return anime.objects.order_by('-pub_date')[:5]
+    
+class DetailView(generic.DetailView):
+    model = anime
+    template_name = 'animeapp/detail.html'
+
+
+    
